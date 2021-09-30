076e92db98503b51ae75dcd84b09f52a38db4ad4
ameralhafid <76484455+ameralhafid@users.noreply.github.com>
2021-09-30 12:55:44 +0200

    start

--- src/Pages/Details/EpisodeDetail.js
+++ src/Pages/Details/EpisodeDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button} from "@material-ui/core";
 import {Link} from "react-router-dom";
 import "./Detail.css";
+import {img_300} from "../../config/config";
 
 export function EpisodeByID({ episode }) {
 
@@ -14,26 +15,34 @@ export function EpisodeByID({ episode }) {
                 </div>
                 <div  className="episodeTitle">
                 <h4> Episode's air date: {episode.air_date} </h4>
-               
+
                 </div>
 
-                
-                  
+
+
                 {episode.characters.map((characters) =>
-                     
-                     <div className="media">  
-                
-                    <div className="subTitle"> 
-                           
+                    <div className="characters">
+
+                     <div className="media">
+
+                    <div className="subTitle">
+
                         <Button  disableElevation>
+                            <img
+                                src={`${img_300}/${characters.id}.jpeg`}
+                                alt={characters.name}
+                                className="ContentModal__portrait"
+                            />
                             <Link exact to={`/character/${characters.id}`}>
                             {characters.name}
                             </Link>
                         </Button>
-                        </div>  
-                        </div>             
+                        </div>
+                        </div>
+                    </div>
                 )}
              </div>
+
 
 
 
